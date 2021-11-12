1c2a51b24519cd69d7e34bda8e3f683e1f43a297
niirjhor <niirjhor@gmail.com>
2021-11-12 18:36:17 +0600

    Done

--- src/Pages/Home/Reviews/ShowReview/ShowReview.js
+++ src/Pages/Home/Reviews/ShowReview/ShowReview.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import Rating from 'react-rating';
+
 
 const ShowReview = (props) => {
-    console.log(props.review);
     const { _id, name, rating, description } = props.review;
+
+
+
     return (
         <div class="col">
             <div class="card h-100">
                 <img width="150px" height="146px" src="images/user-1.png"
                     class="card-img-top w-50 my-5 mx-auto p-3" alt="..." />
                 <div class="card-body text-center">
-                
 
+                    <Rating
+                        initialRating={rating}
+                        emptySymbol="fa fa-star-o fa-2x"
+                        fullSymbol="fa fa-star fa-2x"
+                    />
                     <p class="card-text">{description}</p>
                     <div class="pb-2">
                         <i className="fa fa-star"></i>
